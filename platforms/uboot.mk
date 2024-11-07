eb08f7a7df26bdfa04c156e5f6a5858de2314c9e
Hidemasa Kawasaki <is0553ph@ed.ritsumei.ac.jp>
2024-11-07 21:18:41 +0900

    Add bootcmd for automated execute Bao

--- platforms/uboot.mk
+++ platforms/uboot.mk
@@ -1,9 +1,11 @@
 uboot_repo:=https://github.com/u-boot/u-boot.git
 uboot_version:=v2022.10
 uboot_src:=$(wrkdir_src)/u-boot
+uboot_cfg:=$(bao_demos)/platforms/fvp-a/vexpress_aemv8a_semi_defconfig
 
 $(uboot_src):
 	git clone --depth 1 --branch $(uboot_version) $(uboot_repo) $(uboot_src)
+	cp $(uboot_cfg) $(uboot_src)/configs/vexpress_aemv8a_semi_defconfig
 
 define build-uboot
 $(strip $1): $(uboot_src)
